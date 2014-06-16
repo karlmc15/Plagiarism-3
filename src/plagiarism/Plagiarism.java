1931e350412f32463948583b61e50d7e6ef92c9f
c1343067 <c1343067@10.72.3.46>
2014-06-16 11:00:10 +0100

    first commit in uni

---
+++ src/plagiarism/Plagiarism.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plagiarism;
+
+/**
+ *
+ * @author c1343067
+ */
+public class Plagiarism {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
